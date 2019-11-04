a570b65bed2e0d001650d7c8160a43f901459747
alfianjau <alfianjauhari@gmail.com>
2019-11-04 13:37:11 +0700

    add advanced finder

--- app.js
+++ app.js
@@ -19,3 +19,28 @@ client.connect(function(err) {
 
   client.close()
 })
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents')
+  // Insert some documents
+  collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function(err, result) {
+    assert.equal(err, null)
+    assert.equal(3, result.result.n)
+    assert.equal(3, result.ops.length)
+    console.log('Inserted 3 documents into the collection')
+    callback(result)
+  })
+}
+
+const findDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents')
+  // Find some documents
+  collection.find({ a: 3 }).toArray(function(err, docs) {
+    assert.equal(err, null)
+    console.log('Found the following records')
+    console.log(docs)
+    callback(docs)
+  })
+}
