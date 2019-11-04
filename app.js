e9a56fd86ca6a7c79eea9344030f0ca2c593b8be
alfianjau <alfianjauhari@gmail.com>
2019-11-04 14:14:54 +0700

    add update function

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ client.connect(function(err) {
   const db = client.db(dbName)
 
   insertDocuments(db, function() {
-    findDocuments(db, function() {
+    updateDocuments(db, function() {
       client.close()
     })
   })
@@ -46,5 +46,17 @@ const findDocuments = function(db, callback) {
     console.log('Found the following records')
     console.log(docs)
     callback(docs)
+  })
+}
+
+const updateDocument = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents')
+  // Update document where a is 2, set b equal to 1
+  collection.updateOne({ a: 2 }, { $set: { b: 1 } }, function(err, result) {
+    assert.equal(err, null)
+    assert.equal(1, result.result.n)
+    console.log('Updated the document with the field a equal to 2')
+    callback(result)
   })
 }
