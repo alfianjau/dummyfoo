e7bbfeb26a38937f5874c13c84bcaf29af968ed2
alfianjau <alfianjauhari@gmail.com>
2019-11-04 13:10:38 +0700

    document insertion

--- app.js
+++ app.js
@@ -19,3 +19,16 @@ client.connect(function(err) {
 
   client.close()
 })
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('documents')
+  // Insert some documents
+  collection.insertMany([{ a: 1 }, { a: 2 }, { a: 3 }], function(err, result) {
+    assert.equal(err, null)
+    assert.equal(3, result.result.n)
+    assert.equal(3, result.ops.length)
+    console.log('Inserted 3 documents into the collection')
+    callback(result)
+  })
+}
